78e72f323a22f2e2dca8ade721207601f37c61e1
Dan Rounds <dan.rounds@gmail.com>
2017-03-25 20:47:39 -0400

    Add user feedback, post game

--- js/components/feedback-bar.js
+++ js/components/feedback-bar.js
@@ -17,20 +17,42 @@ export class FeedbackBar extends React.Component {
 }
 
 const mapStateToDisplay = (state, props) => {
-    const difference = Math.abs(state.difference);
     let text;
 
-    if (difference == 100) { text = 'Guess a number between 1 and 100'; } else
-    if (difference > 40)   { text = 'Cold.'; } else
-    if (difference > 30)   { text = 'Like an early-spring breeze...'; } else
-    if (difference > 20)   { text = 'Lukewarm...'; } else
-    if (difference > 15)   { text = 'Warm!'; } else
-    if (difference > 8)    { text = 'Hot!'; } else
-    if (difference > 1)    { text = 'Scorching!'; } else
-    if (difference > 0)    { text = 'You\'re so close!'; } else
-    text = 'You got it!';
-
+    if (!state.gameOver) {
+        text = gameFeedback(Math.abs(state.difference));
+    } else {
+        text = postGameFeedback(state.turnN - state.gameOver);
+    }
     return { text };
 };
+
+const gameFeedback = (difference) => {
+    if (difference == 100) { return 'Guess a number between 1 and 100'; } else
+    if (difference > 40)   { return 'Cold.'; } else
+    if (difference > 30)   { return 'Like an early-spring breeze...'; } else
+    if (difference > 20)   { return 'Lukewarm...'; } else
+    if (difference > 15)   { return 'Warm!'; } else
+    if (difference > 8)    { return 'Hot!'; } else
+    if (difference > 1)    { return 'Scorching!'; } else
+    if (difference > 0)    { return 'You\'re so close!'; }
+};
+
+const postGameFeedback = (turnsPastEnd) => {
+    if (turnsPastEnd < 3) {
+        return 'You got it!'; }
+
+    else if (turnsPastEnd < 12) {
+        const possibilities = [
+            'Yup, you won', 'Game\'s still over', 'Congrats.',
+            'You are the winner', 'Thanks for playing; game over',
+            'Yes...you guessed the right number', 'You are the champions'
+        ];
+        return possibilities[Math.floor(Math.random() * possibilities.length)];
+
+    } else {
+        return 'Click "New Game" to start again';
+    }
+}
 
 export default connect(mapStateToDisplay)(FeedbackBar);
